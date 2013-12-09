45a50222613542c78078725c6aac456f8ca9df97
Werner Wenzel <WernerWenz@gmx.de>
2013-12-09 07:34:38 +0100

    - Bugfix in PrototypeBuilder - Added some Documentation to PrototypeBuilder

--- src/de/se_rwth/codecraft/util/RandomPrototypeBuilder.java
+++ src/de/se_rwth/codecraft/util/RandomPrototypeBuilder.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
+// TODO: Rename, as this is not a Builder pattern, which might be confusing.
+/**
+ * This class realizes a generating system, which clones an arbitrary
+ * number of randomly chosen Prototypes, given to the system.
+ * @author WernerW
+ *
+ * @param <T> The cloneable base Type
+ */
 public class RandomPrototypeBuilder<T extends Cloneable<T>>
 {
 	// We are not expecting many prototypes to be added during runtime thus a simple ArrayList suffices
@@ -13,6 +20,10 @@ public class RandomPrototypeBuilder<T extends Cloneable<T>>
 	// The rng used to generate Creatures
 	private Random _random;
 
+	/**
+	 * Adds a Prototype for cloning
+	 * @param prototype The Prototype to be added
+	 */
 	public void AddPrototype(T prototype)
 	{
 		if (prototype == null)
@@ -20,6 +31,8 @@ public class RandomPrototypeBuilder<T extends Cloneable<T>>
 
 		if (_prototypes.contains(prototype))
 			throw new IllegalArgumentException("Prototype already added");
+
+		_prototypes.add(prototype);
 	}
 
 	public RandomPrototypeBuilder(Random random)
@@ -32,12 +45,21 @@ public class RandomPrototypeBuilder<T extends Cloneable<T>>
 		_random = new Random();
 	}
 
+	/**
+	 * Selects a random Prototype and clones it.
+	 * @return The cloned instance
+	 */
 	public T CreateOne()
 	{
 		int index = _random.nextInt(_prototypes.size());
 		return _prototypes.get(index).Clone();
 	}
 
+	/**
+	 * Selects count times a random Prototype and clones it
+	 * @param count The number of clones to generate
+	 * @return The clones.
+	 */
 	public List<T> Create(int count)
 	{
 		List<T> clones = new ArrayList<T>(count);
